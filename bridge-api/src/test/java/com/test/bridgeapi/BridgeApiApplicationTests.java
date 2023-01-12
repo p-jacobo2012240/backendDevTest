98db94ad4462d7d1093a513cfae8bba05dd90367
p-jacobo2012240 <pablojacobo1998@gmail.com>
2023-01-12 13:52:26 -0600

    initial tooling config

---
+++ bridge-api/src/test/java/com/test/bridgeapi/BridgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.bridgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
